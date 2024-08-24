7d4fbaa82eb676491e9d32dd117d9f8136cf348a
Nilesh Ghule <ghule.nilesh@gmail.com>
2024-08-24 18:34:29 +0530

    Python Variable Demos

---
+++ variables/variables1.py
@@ -0,0 +1,14 @@
+a = 10
+print(f"a = {a}, type of a = {type(a)}")
+
+b = 2.2
+print(f"b = {b}, type of b = {type(b)}")
+
+c = True
+print(f"c = {c}, type of c = {type(c)}")
+
+d = "Nilesh"
+print(f"d = {d}, type of d = {type(d)}")
+
+e = 4 + 6j
+print(f"e = {e}, type of e = {type(e)}")
---
+++ variables/variables2.py
@@ -0,0 +1,5 @@
+num1 = 10
+print(f"num1 type: {type(num1)} | num1 = {num1}")
+
+num1 = 20
+print(f"num1 type: {type(num1)} | num1 = {num1}")
